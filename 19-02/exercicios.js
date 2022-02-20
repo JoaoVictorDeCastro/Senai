2e5b1a4df95a6e848bd385073d5c89714b339b29
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-02-19 23:19:42 -0300

    Create Exercícios

---
+++ 19-02/exercicios.js
@@ -0,0 +1,23 @@
+const nome = 'João Victor'
+console.log(nome)
+
+const clima = 18
+console.log(`${clima}ºC`)
+
+var n1 = 1
+var n2 = 2
+console.log(`A soma entre ${n1} e ${n2} é ${n1+n2}`)
+
+var hora = '12h'
+console.log(`O compromisso será às ${hora}.`)
+
+var idade = 15;
+var moradia = 'Barra do Piraí'
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${moradia}.`)
+
+console.log(`Meu nome é ${nome}`);
+
+console.log(12/'João Victor');
+
+console.log(typeof idade, typeof nome, typeof clima, typeof hora, typeof moradia);
+var estaChovendo = true;
